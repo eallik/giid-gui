591d3ea71762c819ff0e05430213fdc27edd57b9
Erik Allik <eallik@gmail.com>
2016-02-12 16:10:05 +0200

    make a generic selection UI; add nice bg images

--- react-tutorial/public/search.js
+++ react-tutorial/public/search.js
@@ -21,7 +21,7 @@
 // proper components that participate in the hierarchy directly, in which case
 // it is allotted a place in the hierarchy and assigned a view and controller.
 
-var LangBox = React.createClass({
+var SelectBox = React.createClass({
   getInitialState: function() { return {isModifying: false, selected: this.props.selected}; },
 
   onBeginSelection: function(e) { this.setState({isModifying: true}); },
@@ -32,18 +32,18 @@ var LangBox = React.createClass({
     this.setState({selected: values});
   },
   render: function() {
-    var langsSelect = (
-        <LangsDoSelect
+    var picker = (
+        <SelectPicker
           selected={this.state.selected} all={this.props.all}
           onYield={this.onYieldSelection}
           onDone={this.onEndSelection}
         />
     );
-    var selectedItems = this.state.selected.map(x => <img width="40px" height="28px" style={{padding: "2px"}} key={x.code} alt={x.label} src={x.iconUri()} />);
+    var selectedItems = this.state.selected.map(x => <img width="40px" height="28px" style={{padding: "2px"}} key={x.key} alt={x.label} src={x.iconUri()} />);
     return (
-        <div className="lang-box" style={{position: "relative"}}>
+        <div className="select-box" style={{position: "relative"}}>
           <div style={{position: "absolute", zIndex: navigator.platform === "iPhone" ? -1 : 2}}>
-            {this.state.isModifying ? langsSelect : null}
+            {this.state.isModifying ? picker : null}
           </div>
           <div style={{position: "absolute", zIndex:  1}} onClick={this.onBeginSelection}>
             {selectedItems}
@@ -54,25 +54,22 @@ var LangBox = React.createClass({
 });
 
 
-var LangsDoSelect = React.createClass({
+var SelectPicker = React.createClass({
   getInitialState: function() { return {selected: this.props.selected}; },
   componentDidMount: function() { this.refs.select.focus(); },
-  isLangSelected: function(code) {
-    return this.props.selected.some(x => x.code === code);
-  },
+  isSelected: function(key) { return this.props.selected.some(x => x.key === key); },
   onChange: function(e) {
-    var vals = $(e.target).val().map(code => this.props.all.find(x => x.code === code));
-    console.debug("onChange: ", vals);
+    var vals = $(e.target).val().map(key => this.props.all.find(x => x.key === key));
     this.setState({selected: vals});
     this.props.onYield(vals);
   },
   render: function() {
     var options = this.props.all.map(
-      ({code,label}) => <option key={code} value={code}>{label}</option>
+      ({key,label}) => <option key={key} value={key}>{label}</option>
     );
     // XXX: the dummy optgroup is needed; otherwise iOS safari auto-unselects the first value for some reason
     return (
-        <select style={{appearance: "none"}} ref="select" value={this.state.selected.map(x => x.code)} onChange={this.onChange} className="langs-do-select" multiple="multiple" onBlur={this.props.onDone}>
+        <select style={{appearance: "none"}} ref="select" value={this.state.selected.map(x => x.key)} onChange={this.onChange} className="select-picker" multiple="multiple" onBlur={this.props.onDone}>
           <optgroup disabled></optgroup>
           {options}
         </select>
@@ -84,18 +81,28 @@ var LangsDoSelect = React.createClass({
 //////////////////////////
 
 var Search = React.createClass({
-  style: {
-    backgroundColor: "yellow",
-    height: "100%"
+  style: { height: "100%" },
+  bgStyle: {
+    backgroundImage: (new Date()).getMinutes() % 2 == 0 ? "url(/img/bg_rome3.jpg)" : "url(/img/bg_paris.jpg)",
+    backgroundPosition: "center center", backgroundSize: "auto 100%", backgroundRepeat: "no-repeat",
+    WebkitFilter: "blur(5px)",
+
+    position: "fixed", left: 0, right: 0, top: 0, bottom: 0, zIndex: 1
+  },
+  contentStyle: {
+    position: "fixed", left: 0, right: 0, zIndex: 9999
   },
   render: function() {
-    var selected = [
+    var selectedLangs = [
       _LANG_ALL_LOOKUP["en"],
       _LANG_ALL_LOOKUP["et"]
     ];
     return (
       <div className="search" style={this.style}>
-        <LangBox selected={selected} all={LANG_ALL} />
+        <div style={this.bgStyle}></div>
+        <div style={this.contentStyle}>
+          <SelectBox selected={selectedLangs} all={LANG_ALL} />
+        </div>
       </div>
     );
   }
@@ -115,26 +122,26 @@ Language.prototype = {
     return this.icoUriWithGeom({w:128,h:128});
   },
   icoUriWithGeom: function(geom) {
-    return ("/img/" + this.code + "_" + this.ctryCodes.join('+')
+    return ("/img/" + this.key + "_" + this.ctryCodes.join('+')
             + "_" + geom.w+"x"+geom.h + ".png");
   }
 }
 
 var LANG_ALL = [
-  {code: "en", ctryCodes: ["us", "uk"], label: "English"},
-  {code: "et", ctryCodes: ["ee"],       label: "Eesti keel"},
-  {code: "fi", ctryCodes: ["fi"],       label: "Suomi"},
-  {code: "ru", ctryCodes: ["rf"],       label: "Ру́сский"},
-  {code: "sw", ctryCodes: ["se"],       label: "Svenska"},
-  {code: "de", ctryCodes: ["de"],       label: "Deutsch"},
-  {code: "fr", ctryCodes: ["fr"],       label: "Français"},
-  {code: "es", ctryCodes: ["es"],       label: "Español"},
-  {code: "it", ctryCodes: ["it"],       label: "Italiano"},
-  {code: "pt", ctryCodes: ["pt", "br"],       label: "Português"},
+  {key: "en", ctryCodes: ["us", "uk"], label: "English"},
+  {key: "et", ctryCodes: ["ee"],       label: "Eesti keel"},
+  {key: "fi", ctryCodes: ["fi"],       label: "Suomi"},
+  {key: "ru", ctryCodes: ["rf"],       label: "Ру́сский"},
+  {key: "sw", ctryCodes: ["se"],       label: "Svenska"},
+  {key: "de", ctryCodes: ["de"],       label: "Deutsch"},
+  {key: "fr", ctryCodes: ["fr"],       label: "Français"},
+  {key: "es", ctryCodes: ["es"],       label: "Español"},
+  {key: "it", ctryCodes: ["it"],       label: "Italiano"},
+  {key: "pt", ctryCodes: ["pt", "br"],       label: "Português"},
 ].map(y => new Language(y));
 
 var _LANG_ALL_LOOKUP = mapFromList(
-  LANG_ALL.map(y => [y.code, y])
+  LANG_ALL.map(y => [y.key, y])
 );
 
 window.LANG_ALL = LANG_ALL;
